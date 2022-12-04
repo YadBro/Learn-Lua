37f513ff9ff130c2aee08d913bed580dfbefdbbc
Yadi Apriyadi <ncada708@gmail.com>
2022-12-05 05:44:41 +0700

    metatable in lua

--- 12 - metatable/main.lua
+++ 12 - metatable/main.lua
@@ -1,5 +1,5 @@
 local function sum(num1, num2)
-  return num1 + num2
+  return num1.num + num2.num
 end
 
 local metatable = {
@@ -11,4 +11,4 @@ local table2 = { num = 2 }
 
 setmetatable(table1, metatable)
 
-print(table1.num + table2.num)
+print(table1 + table2)
