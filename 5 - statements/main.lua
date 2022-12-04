126f0121a58e0032d279a4784a8a7b555e94f5ed
Yadi Apriyadi <ncada708@gmail.com>
2022-12-04 15:12:42 +0700

    statements in lua

---
+++ 5 - statements/main.lua
@@ -0,0 +1,10 @@
+local adult, young = 18, 14
+local age = 18
+-- adult, young, baby
+if (age >= adult) then
+  print('He is adult')
+elseif (age >= young) then
+  print('He is young')
+else
+  print('He is baby')
+end
