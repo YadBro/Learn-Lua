0992c15856bd7d1a670a50e4feb38024af64244f
Yadi Apriyadi <ncada708@gmail.com>
2022-12-05 05:45:01 +0700

    update

--- 12 - metatable/main.lua
+++ 12 - metatable/main.lua
@@ -2,8 +2,13 @@ local function sum(num1, num2)
   return num1.num + num2.num
 end
 
+local function substraction(num1, num2)
+  return num1.num + num2.num
+end
+
 local metatable = {
-  __add = sum
+  __add = sum,
+  __sub = substraction
 }
 
 local table1 = { num = 1 }
@@ -11,4 +16,4 @@ local table2 = { num = 2 }
 
 setmetatable(table1, metatable)
 
-print(table1 + table2)
+print(table1 - table2)
