ee8ce45328844bcf164fe956cf14d75105adffb7
Yadi Apriyadi <ncada708@gmail.com>
2022-12-05 02:20:07 +0700

    add recursive function

--- 6 - functions/main.lua
+++ 6 - functions/main.lua
@@ -15,3 +15,18 @@ local function test(num1, ...)
 end
 
 print(test(1, 2, 5, 5, 5, 10, 5, 5))
+
+-- Recursive function
+print('\n Recursive function')
+local function counter(number, endNumber)
+  local count = number + 1
+
+  if count < endNumber then
+    print('counter at ' .. count)
+    return counter(count, endNumber)
+  end
+
+  return count
+end
+
+print(counter(1, 10))
